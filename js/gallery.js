d13aee659e12e829d1112b28fbb59b66b62fa373
Japneet Singh Baluja <96378977+JapneetSinghh@users.noreply.github.com>
2022-09-01 00:03:04 +0530

    Add files via upload

--- js/gallery.js
+++ js/gallery.js
@@ -1,29 +1,440 @@
-
-const GallerySource=[
+const GallerySource = [
   {
-    type:'lobby',
-    src:'./img/gallery/1.jpg',
-    alt:''
+    id:'',
+    type: 'Food',
+    src: './img/Food/Gurukul-pg-food-3.jpg',
+    alt: ''
+  },
+  { 
+    id:'',
+    type: 'Single-Occupancy-Room',
+    src: './img/Single Occupancy/Gurukul-pg-uttamNagar-Janakpuri-single-occupancy-2.jpg',
+    alt: ''
+  },
+  { 
+    id:'',
+    type: 'Double-Occupancy-Room',
+    src: './img/Double Occupancy/Gurukul-pg-uttamNagar-Janakpuri-double-occupancy-11.jpg',
+    alt: ''
   },
   {
-    type:'Washroom',
-    src:'./img/gallery/2.jpg',
-    alt:''
+    id:'',
+    type: 'Amenities',
+    src: './img/Gym/Gurukul_pg_uttamNagar_janakpuri_gym5.jpg',
+    alt: ''
   },
   {
-    type:'Single-Occupancy-Room',
-    src:'./img/gallery/3.jpg',
-    alt:''
+    id:'',
+    type: 'Lobby',
+    src: './img/Lobby/Pg_in_delhi_janakpuri_lobby-5.jpg',
+    alt: ''
   },
   {
-    type:'Double-Occupancy-Room',
-    src:'./img/gallery/4.jpg',
-    alt:''
+    id:'',
+    type: 'Outside-PG',
+    src: './img/Building/Gurukul-pg-building-2.jpg',
+    alt: ''
   },
+  {
+    id:'',
+    type: 'Washrooms',
+    src: './img/Washroom/Gurukul-pg-uttamNagar-janakpuri-washrooms-8.jpg',
+    alt: ''
+  },
+  {
+    id:'',
+    type: 'Roof-&-Seating-Area',
+    src: './img/Roof/Gurukul_pg_uttamNagar_janakpuri_roof2.jpg',
+    alt: ''
+  },
+  {
+    id:'',
+    type: 'Double-Occupancy-Room',
+    src: './img/Double Occupancy/Gurukul-pg-uttamNagar-Janakpuri-double-occupancy-4.jpg',
+    alt: ''
+  },
+  {
+    id:'',
+    type: 'Double-Occupancy-Room',
+    src: './img/Double Occupancy/Gurukul-pg-uttamNagar-Janakpuri-double-occupancy-2.jpg',
+    alt: ''
+  },
+  {
+    id:'',
+    type: 'Double-Occupancy-Room',
+    src: './img/Double Occupancy/Gurukul-pg-uttamNagar-Janakpuri-double-occupancy-5.jpg',
+    alt: ''
+  },
+  {
+    id:'',
+    type: 'Double-Occupancy-Room',
+    src: './img/Double Occupancy/Gurukul-pg-uttamNagar-Janakpuri-double-occupancy-7.jpg',
+    alt: ''
+  },
+  {
+    id:'',
+    type: 'Double-Occupancy-Room',
+    src: './img/Double Occupancy/Gurukul-pg-uttamNagar-Janakpuri-double-occupancy-9.jpg',
+    alt: ''
+  },
+  {
+    id:'',
+    type: 'Double-Occupancy-Room',
+    src: './img/Double Occupancy/Gurukul-pg-uttamNagar-Janakpuri-double-occupancy-10.jpg',
+    alt: ''
+  },
+  {
+    id:'',
+    type: 'Double-Occupancy-Room',
+    src: './img/Double Occupancy/Gurukul-pg-uttamNagar-Janakpuri-double-occupancy-11.jpg',
+    alt: ''
+  },
+  {
+    id:'',
+    type: 'Double-Occupancy-Room',
+    src: './img/Double Occupancy/Gurukul-pg-uttamNagar-Janakpuri-double-occupancy-6.jpg',
+    alt: ''
+  },
+  {
+    id:'',
+    type: 'Double-Occupancy-Room',
+    src: './img/Double Occupancy/Gurukul-pg-uttamNagar-Janakpuri-double-occupancy-12.jpg',
+    alt: ''
+  },
+  {
+    id:'',
+    type: 'Double-Occupancy-Room',
+    src: './img/Double Occupancy/Gurukul-pg-uttamNagar-Janakpuri-double-occupancy-13.jpg',
+    alt: ''
+  },
+  {
+    id:'',
+    type: 'Double-Occupancy-Room',
+    src: './img/Double Occupancy/Gurukul-pg-uttamNagar-Janakpuri-double-occupancy-18.jpg',
+    alt: ''
+  },
+  {
+    id:'',
+    type: 'Double-Occupancy-Room',
+    src: './img/Double Occupancy/Gurukul-pg-uttamNagar-Janakpuri-double-occupancy-17.jpg',
+    alt: ''
+  },
+  {
+    id:'',
+    type: 'Double-Occupancy-Room',
+    src: './img/Double Occupancy/Gurukul-pg-uttamNagar-Janakpuri-double-occupancy-20.jpg',
+    alt: ''
+  },
+  {
+    id:'',
+    type: 'Double-Occupancy-Room',
+    src: './img/Double Occupancy/Gurukul-pg-uttamNagar-Janakpuri-double-occupancy-19.jpg',
+    alt: ''
+  },
+  {
+    id:'',
+    type: 'Double-Occupancy-Room',
+    src: './img/Double Occupancy/Gurukul-pg-uttamNagar-Janakpuri-double-occupancy-21.jpg',
+    alt: ''
+  },
+  {
+    id:'',
+    type: 'Double-Occupancy-Room',
+    src: './img/Double Occupancy/Gurukul-pg-uttamNagar-Janakpuri-double-occupancy-22.jpg',
+    alt: ''
+  },
+  {
+    id:'',
+    type: 'Food',
+    src: './img/Food/Gurukul-pg-food-1.jpg',
+    alt: ''
+  },
+  {
+    id:'',
+    type: 'Food',
+    src: './img/Food/Gurukul-pg-food-5.jpg',
+    alt: ''
+  },
+  {
+    id:'',
+    type: 'Food',
+    src: './img/Food/Gurukul-pg-food-4.jpg',
+    alt: ''
+  },
+  {
+    id:'',
+    type: 'Roof-&-Seating-Area',
+    src: './img/Roof/Gurukul_pg_uttamNagar_janakpuri_roof1.jpg',
+    alt: ''
+  },
+  {
+    id:'',
+    type: 'Single-Occupancy-Room',
+    src: './img/Single Occupancy/Gurukul-pg-uttamNagar-Janakpuri-single-occupancy-1.jpg',
+    alt: ''
+  },
+  {
+    id:'',
+    type: 'Single-Occupancy-Room',
+    src: './img/Single Occupancy/Gurukul-pg-uttamNagar-Janakpuri-single-occupancy-3.jpg',
+    alt: ''
+  }, 
+  {
+    id:'',
+    type: 'Single-Occupancy-Room',
+    src: './img/Single Occupancy/Gurukul-pg-uttamNagar-Janakpuri-single-occupancy-5.jpg',
+    alt: ''
+  }, 
+  {
+    id:'',
+    type: 'Single-Occupancy-Room',
+    src: './img/Single Occupancy/Gurukul-pg-uttamNagar-Janakpuri-single-occupancy-6.jpg',
+    alt: ''
+  }, 
+  {
+    id:'',
+    type: 'Single-Occupancy-Room',
+    src: './img/Single Occupancy/Gurukul-pg-uttamNagar-Janakpuri-single-occupancy-7.jpg',
+    alt: ''
+  }, 
+  {
+    id:'',
+    type: 'Single-Occupancy-Room',
+    src: './img/Single Occupancy/Gurukul-pg-uttamNagar-Janakpuri-single-occupancy-8.jpg',
+    alt: ''
+  }, 
+  {
+    id:'',
+    type: 'Single-Occupancy-Room',
+    src: './img/Single Occupancy/Gurukul-pg-uttamNagar-Janakpuri-single-occupancy-9.jpg',
+    alt: ''
+  },
+  {
+    id:'',
+    type: 'Single-Occupancy-Room',
+    src: './img/Single Occupancy/Gurukul-pg-uttamNagar-Janakpuri-single-occupancy-10.jpg',
+    alt: ''
+  }, 
+  {
+    id:'',
+    type: 'Single-Occupancy-Room',
+    src: './img/Single Occupancy/Gurukul-pg-uttamNagar-Janakpuri-single-occupancy-11.jpg',
+    alt: ''
+  },
+  {
+    id:'',
+    type: 'Single-Occupancy-Room',
+    src: './img/Single Occupancy/Gurukul-pg-uttamNagar-Janakpuri-single-occupancy-13.jpg',
+    alt: ''
+  },
+  {
+    id:'',
+    type: 'Single-Occupancy-Room',
+    src: './img/Single Occupancy/Gurukul-pg-uttamNagar-Janakpuri-single-occupancy-14.jpg',
+    alt: ''
+  },
+  {
+    id:'',
+    type: 'Single-Occupancy-Room',
+    src: './img/Single Occupancy/Gurukul-pg-uttamNagar-Janakpuri-single-occupancy-15.jpg',
+    alt: ''
+  },
+  {
+    id:'',
+    type: 'Single-Occupancy-Room',
+    src: './img/Single Occupancy/Gurukul-pg-uttamNagar-Janakpuri-single-occupancy-16.jpg',
+    alt: ''
+  },
+  {
+    id:'',
+    type: 'Single-Occupancy-Room',
+    src: './img/Single Occupancy/Gurukul-pg-uttamNagar-Janakpuri-single-occupancy-17.jpg',
+    alt: ''
+  },
+  {
+    id:'',
+    type: 'Washrooms',
+    src: './img/Washroom/Gurukul-pg-uttamNagar-janakpuri-washrooms-2.jpg',
+    alt: ''
+  },
+  {
+    id:'',
+    type: 'Washrooms',
+    src: './img/Washroom/Gurukul-pg-uttamNagar-janakpuri-washrooms-3.jpg',
+    alt: ''
+  },
+  {
+    id:'',
+    type: 'Washrooms',
+    src: './img/Washroom/Gurukul-pg-uttamNagar-janakpuri-washrooms-4.jpg',
+    alt: ''
+  },
+  {
+    id:'',
+    type: 'Washrooms',
+    src: './img/Washroom/Gurukul-pg-uttamNagar-janakpuri-washrooms-6.jpg',
+    alt: ''
+  },
+  {
+    id:'',
+    type: 'Washrooms',
+    src: './img/Washroom/Gurukul-pg-uttamNagar-janakpuri-washrooms-7.jpg',
+    alt: ''
+  },
+  {
+    id:'',
+    type: 'Washrooms',
+    src: './img/Washroom/Gurukul-pg-uttamNagar-janakpuri-washrooms-10.jpg',
+    alt: ''
+  },
+  {
+    id:'',
+    type: 'Washrooms',
+    src: './img/Washroom/Gurukul-pg-uttamNagar-janakpuri-washrooms-11.jpg',
+    alt: ''
+  },
+  {
+    id:'',
+    type: 'Outside-PG',
+    src: './img/Building/Gurukul-pg-building-1.jpg',
+    alt: ''
+  },
+  {
+    id:'',
+    type: 'Outside-PG',
+    src: './img/Building/Gurukul-pg-building-3.jpg',
+    alt: ''
+  },
+  {
+    id:'',
+    type: 'Outside-PG',
+    src: './img/Roof/Gurukul_pg_uttamNagar_janakpuri_roof1.jpg',
+    alt: ''
+  },
+  {
+    id:'',
+    type: 'Outside-PG',
+    src: './img/Metro/Gurukul_pg_uttamNagar_metro_5.jpg',
+    alt: ''
+  },  
+  {
+    id:'',
+    type: 'Amenities',
+    src: './img/Gym/Gurukul_pg_uttamNagar_janakpuri_gym3.jpg',
+    alt: ''
+  },
+  {
+    id:'',
+    type: 'Roof-&-Seating-Area',
+    src: './img/Rest/Gurukul-pg-amenities-2.jpg',
+    alt: ''
+  },
+  {
+    id:'',
+    type: 'Roof-&-Seating-Area',
+    src: './img/Rest/Gurukul-pg-amenities-1.jpg',
+    alt: ''
+  },
+  {
+    id:'',
+    type: 'Amenities',
+    src: './img/Rest/Gurukul-pg-amenities-9.jpg',
+    alt: ''
+  },
+  {
+    id:'',
+    type: 'Amenities',
+    src: './img/Rest/Gurukul-pg-amenities-7.jpg',
+    alt: ''
+  },
+  {
+    id:'',
+    type: 'Amenities',
+    src: './img/Rest/Gurukul-pg-amenities-11.jpg',
+    alt: ''
+  },
+  {
+    id:'',
+    type: 'Amenities',
+    src: './img/Rest/Gurukul-pg-amenities-12.jpg',
+    alt: ''
+  },
+  {
+    id:'',
+    type: 'Amenities',
+    src: './img/Rest/Gurukul-pg-amenities-13.jpg',
+    alt: ''
+  },
+  {
+    id:'',
+    type: 'Amenities',
+    src: './img/Washroom/Gurukul-pg-uttamNagar-janakpuri-washrooms-9.jpg',
+    alt: ''
+  },
+  {
+    id:'',
+    type: 'Amenities',
+    src: './img/Roof/Gurukul_pg_uttamNagar_janakpuri_roof2.jpg',
+    alt: ''
+  },
+  {
+    id:'',
+    type: 'Amenities',
+    src: './img/Lobby/Pg_in_delhi_janakpuri_lobby-6.jpg',
+    alt: ''
+  },
+  {
+    id:'',
+    type: 'Amenities',
+    src: './img/Lobby/Pg_in_delhi_janakpuri_lobby-3.jpg',
+    alt: ''
+  },
+  {
+    id:'',
+    type: 'Lobby',
+    src: './img/Lobby/Pg_in_delhi_janakpuri_lobby-1.jpg',
+    alt: ''
+  },
+  {
+    id:'',
+    type: 'Lobby',
+    src: './img/Lobby/Pg_in_delhi_janakpuri_lobby-2.jpg',
+    alt: ''
+  },
+  {
+    id:'',
+    type: 'Lobby',
+    src: './img/Lobby/Pg_in_delhi_janakpuri_lobby-7.jpg',
+    alt: ''
+  },
+  {
+    id:'',
+    type: 'Lobby',
+    src: './img/Lobby/Pg_in_delhi_janakpuri_lobby-8.jpg',
+    alt: ''
+  },
+  {
+    id:'',
+    type: 'Lobby',
+    src: './img/Lobby/Pg_in_delhi_janakpuri_lobby-9.jpg',
+    alt: ''
+  },
+  {
+    id:'',
+    type: 'Lobby',
+    src: './img/Rest/Gurukul-pg-amenities-4.jpg',
+    alt: ''
+  },  {
+    id:'',
+    type: 'Lobby',
+    src: './img/Food/Gurukul-pg-food-1.jpg',
+    alt: ''
+  },
+  
 ]
-const galleryContainer=document.querySelector('#gallery-images');
+const galleryContainer = document.querySelector('#gallery-images');
 // console.log(galleryContainer);
-const btnGalleryContainer=document.querySelector('.gallery-types');
+const btnGalleryContainer = document.querySelector('.gallery-types');
 
 
 displayGalleryMenu();
@@ -48,57 +459,57 @@ function displayAllImages(galleryItems) {
 
 
 // DISPLAYING GALLERY NAVBAR BTNS
-function displayGalleryMenu(){
-// CREATING ARRAY FOR BTN TYPES
-const galleyCategories=GallerySource.reduce(function(values,item){
-  if(!values.includes(item.type)){
-    values.push(item.type);
-    // console.log(values);
-   }
-   return values;  
- },
- ["All"]
- );
-// console.log(galleyCategories);
+function displayGalleryMenu() {
+  // CREATING ARRAY FOR BTN TYPES
+  const galleyCategories = GallerySource.reduce(function (values, item) {
+    if (!values.includes(item.type)) {
+      values.push(item.type);
+      // console.log(values);
+    }
+    return values;
+  },
+    ["All"]
+  );
+  // console.log(galleyCategories);
 
 
-// ADDING ARRAY DATA TO NAVBAR AS BTNS
-const categoryBtns = galleyCategories
-  .map(function (btn) {
-    return `<li><button type="button" onclick="sleep()" class="btn" data-id=${btn}>
+  // ADDING ARRAY DATA TO NAVBAR AS BTNS
+  const categoryBtns = galleyCategories
+    .map(function (btn) {
+      return `<li><button type="button" onclick="sleep()" class="btn" data-id=${btn}>
         ${btn}
       </button><li>`;
-  })
-  .join("");
+    })
+    .join("");
 
   // console.log(categoryBtns);
-  btnGalleryContainer.innerHTML=categoryBtns;
-  const filterGalleryBtns=btnGalleryContainer.querySelectorAll('.btn');
-  filterGalleryBtns.forEach(function(btn){
-   
-    btn.addEventListener("click",function(e){
-  
+  btnGalleryContainer.innerHTML = categoryBtns;
+  const filterGalleryBtns = btnGalleryContainer.querySelectorAll('.btn');
+  filterGalleryBtns.forEach(function (btn) {
 
-      filterGalleryBtns.forEach((btn2)=>{
-        btn2.style.background='none';
-        btn2.style.borderColor='white';
-       })
-      btn.style.background='var(--mustard)';
-      btn.style.borderColor='var(--mustard)';
-      const type=e.currentTarget.dataset.id;
-      const galleryCat=GallerySource.filter(function(list){
-        if(list.type === type){
+    btn.addEventListener("click", function (e) {
+
+
+      filterGalleryBtns.forEach((btn2) => {
+        btn2.style.background = 'none';
+        btn2.style.borderColor = 'white';
+      })
+      btn.style.background = 'var(--mustard)';
+      btn.style.borderColor = 'var(--mustard)';
+      const type = e.currentTarget.dataset.id;
+      const galleryCat = GallerySource.filter(function (list) {
+        if (list.type === type) {
           return list;
         }
       });
       if (type === "All") {
-       displayAllImages(GallerySource);
-     } else {
-       displayAllImages(galleryCat);
-     }
- 
+        displayAllImages(GallerySource);
+      } else {
+        displayAllImages(galleryCat);
+      }
+
     });
- 
+
   });
 
 
--- js/galleryHome.js
+++ js/galleryHome.js
@@ -1,33 +1,33 @@
 const galleryHome=[
   {
-    src:'./img/testimonials/1.jpg'
+    src:'./img/Building/Gurukul-pg-building-2.jpg'
   },
   {
-    src:'./img/testimonials/2.jpg'
+    src:'./img/Double Occupancy/Gurukul-pg-uttamNagar-Janakpuri-double-occupancy-11.jpg'
   },
   {
-    src:'./img/testimonials/3.jpg'
+    src:'./img/Single Occupancy/Gurukul-pg-uttamNagar-Janakpuri-single-occupancy-11.jpg'
+  },  
+  {
+    src:'./img/Lobby/Pg_in_delhi_janakpuri_lobby-5.jpg'
+  }, 
+  {
+    src:'./img/Washroom/Gurukul-pg-uttamNagar-janakpuri-washrooms-8.jpg'
+  }, 
+  {
+    src:'./img/Food/Gurukul-pg-food-4.jpg'
+  }, 
+  {
+    src:'./img/Gym/Gurukul_pg_uttamNagar_janakpuri_gym5.jpg'
+  }, 
+  {
+    src:'./img/Single Occupancy/Gurukul-pg-uttamNagar-Janakpuri-single-occupancy-13.jpg'
   },
   {
-    src:'./img/testimonials/4.jpg'
+    src:'./img/Food/Gurukul-pg-food-2.jpg'
   },
   {
-    src:'./img/testimonials/5.jpg'
-  },
-  {
-    src:'./img/testimonials/1.jpg'
-  },
-  {
-    src:'./img/testimonials/2.jpg'
-  },
-  {
-    src:'./img/testimonials/3.jpg'
-  },
-  {
-    src:'./img/testimonials/4.jpg'
-  },
-  {
-    src:'./img/testimonials/5.jpg'
+    src:'./img/Rest/Gurukul-pg-amenities-9.jpg'
   }
 ]
 
